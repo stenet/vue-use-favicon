9204b69e52c31255d143f8b637a13a01ca1ab5dd
Stefan Heim <stefan.heim@tip.co.at>
2022-08-02 15:06:47 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+import { useFavicon } from "@vueuse/core";
+
+const favicon = useFavicon();
+favicon.value = "https://www.google.at/favicon.ico";
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
